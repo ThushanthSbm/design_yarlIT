e80b5949e722d8ebda9b365998bb7fd90d57a458
ThushanthSbm <56014745+ThushanthSbm@users.noreply.github.com>
2021-12-18 21:09:40 +0530

    layouts & pages connect

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color:blue;
+        }
+        .active{
+            background:red;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr height="100" style="background:red">
+            <td colspan="2">Header</td>
+        </tr>
+        <tr height="400" >
+            <td width="20%" style="background:blue"><ul>
+                <li class="{{Request::is('/')?'active':''}}"><a href="{{url('/')}}">Home</a></li>
+                <li class="{{Request::is('about')?'active':''}}"><a href="{{url('/about')}}">About</a></li>
+                <li><a href="{{route('home')}}">Home</a></li>
+                <li><a href="{{route('about')}}">About</a></li>
+                <li><a href="{{url('/')}}">home</a></li>
+            </ul></td>
+        <td>
+            @yield('content')
+        </td></tr>
+        <tr height="100" style="background:red">
+    <td colspan="2">footer</td></tr>
+    </table>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+<h1>Welcome Home page</h1>
+@endsection
